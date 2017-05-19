9125aaae75aa7d473f378084590335a7734017f0
Drew Budwin <drew@budw.in>
2017-05-19 14:51:24 -0400

    Creating Packer template for creating snapshot on Digital Ocean
    Created using tutorials with some modifications:
    
    https://www.digitalocean.com/community/tutorials/how-to-create-digitalocean-snapshots-using-packer-on-ubuntu-16-04
    https://www.digitalocean.com/community/tutorials/how-to-install-nginx-on-ubuntu-16-04
    
    I ran `brew install packer` to install Packer on my machine.
    
    From the 'packer' directory, validate the template by doing...
    
    `packer validate -var-file=variables.json template.json`
    
    From the 'packer' directory, build the tempalte by doing...
    
    `packer build -var-file=variables.json template.json`
    
    The main functional change I made to the tutorial was to use the `sites-enabled` folder instead of placing server blocks in `/etc/nginx/vhost.d/*.conf`
    
    After turning the image into a droplet, run the following command and then restart Nginx:
    
    `sudo sed -i 's/^.*server_name example.com/server_name [IP OF DROPLET]/' /etc/nginx/sites-available/example.com`

---
+++ packer/scripts/configureNginx.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Script to install and setup Nginx and enable on boot.
+
+# Update system
+apt-get update -y
+apt-get upgrade -y
+
+# Install Nginx:
+apt-get install -y nginx
+
+# Start Nginx service and enable to start on boot
+systemctl enable nginx
+systemctl start nginx
+
+# Create a backup of the original configuration files and import configuration
+cp /etc/nginx/nginx.conf /etc/nginx/nginx.conf.original
+cp /tmp/nginx.conf /etc/nginx/nginx.conf
+
+# Setup a website with the desired server block and permissions
+mkdir -p /var/www/html/example.com
+chown -R www-data:www-data /var/www/html/example.com
+chmod 755 /var/www
+cp /tmp/example.com/example.com /etc/nginx/sites-available/example.com
+
+# Activate the host by creating a symbolic link between the sites-available directory and the sites-enabled directory
+ln -s /etc/nginx/sites-available/example.com /etc/nginx/sites-enabled/example.com
+
+# Delete the default nginx server block
+rm /etc/nginx/sites-enabled/default
+
+# Copy over the html test page:
+cp /tmp/example.com/index.html /var/www/html/example.com/index.html
+
+# Restart Nginx:
+systemctl restart nginx
